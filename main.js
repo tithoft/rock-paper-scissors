a2fb4b90806aae07c0e2f7bed86d083f113c1161
tithoft <99927615+tithoft@users.noreply.github.com>
2025-09-05 12:43:50 -0400

    Added comparison forumals to determine winner and playRound function.

--- main.js
+++ main.js
@@ -1,6 +1,42 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice () {
     const choices = ["rock", "paper", "scissors"];
-    return Math.floor(Math.random() * 3);
+    let computerChoice = Math.floor(Math.random() * choices.length);
+    if(computerChoice === 0) {
+        return "rock";
+    } else if(computerChoice === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
 
 console.log(getComputerChoice());
+
+function getHumanChoice () {
+    let humanChoice = prompt("Rock, Paper, or Scissors?");
+    return humanChoice.toLowerCase();
+}
+
+console.log(getHumanChoice());
+
+function playRound (humanChoice, computerChoice) {
+    if(humanChoice === computerChoice) {
+        return "It's a Tie!";
+    } else if(
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        return "You Win!";
+    } else {
+        return "The Computer Wins!";
+    }
+}
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
