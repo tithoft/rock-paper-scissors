db83f771429cc9307180095b98b62d7fea385b3d
tithoft <99927615+tithoft@users.noreply.github.com>
2025-09-09 13:09:15 -0400

    Function created to display results.
    Function created to determine winner.

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ let computerScore = 0;
 const rockBtn = document.querySelector("#rockBtn");
 const paperBtn = document.querySelector("#paperBtn");
 const scissorsBtn = document.querySelector("#scissorsBtn");
+const resultsDiv = document.querySelector("#results");
+const scoreDiv = document.querySelector("#score");
 
 function getComputerChoice () {
     const choices = ["rock", "paper", "scissors"];
@@ -17,33 +19,38 @@ function getComputerChoice () {
     }
 }
 
-function getHumanChoice () {
-    let humanChoice = prompt("Rock, Paper, or Scissors?");
-    return humanChoice.toLowerCase();
+function playRound (humanChoice) {
+    const computerChoice = getComputerChoice();
+    let roundResult = "";
+
+    if(humanChoice === computerChoice) {
+        roundResult = "It's a Tie!";
+    } else if(
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        humanScore = humanScore + 1;
+        roundResult = "You Win, " + humanChoice + " beats " + computerChoice + "!";
+    } else {
+        computerScore = computerScore + 1;
+        roundResult = "The Computer Wins, " + computerChoice + " beats " + humanChoice + "!";
+    }
+
+    displayResults(roundResult);
 }
 
-//function playGame() {
+function displayResults(result) {
+    resultsDiv.textContent = result;
+    scoreDiv.textContent = `Player: ${humanScore} | Computer: ${computerScore}`;
 
-    function playRound (humanChoice) {
-        const computerChoice = getComputerChoice();
-        if(humanChoice === computerChoice) {
-            return "It's a Tie!";
-        } else if(
-            (humanChoice === "rock" && computerChoice === "scissors") ||
-            (humanChoice === "paper" && computerChoice === "rock") ||
-            (humanChoice === "scissors" && computerChoice === "paper")
-        ) {
-            humanScore = humanScore + 1;
-            return "You Win, " + humanChoice + " beats " + computerChoice + "!";
-        } else {
-            computerScore = computerScore + 1;
-            return "The Computer Wins, " + computerChoice + " beats " + humanChoice + "!";
-        }
+    if (humanScore === 5) {
+        resultsDiv.textContent = "You are the Winner!";
+    } else if (computerScore === 5) {
+        resultsDiv.textContent = "The Computer is the Winner!";
     }
-//}
+}
 
-rockBtn.addEventListener("click", (event) => console.log("Button clicked"));
-paperBtn.addEventListener("click", () => playRound("papaer"));
+rockBtn.addEventListener("click", (event) => playRound("rock"));
+paperBtn.addEventListener("click", () => playRound("paper"));
 scissorsBtn.addEventListener("click", () => playRound("scissors"));
-
-//playGame();
